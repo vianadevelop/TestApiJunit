8766470b0546da54f91454391f1140936cd1cbb0
Edward Gomez <edward25nov@gmail.com>
2022-03-24 17:17:38 -0600

    estructura dummy

--- src/test/java/app/BookingTest.java
+++ src/test/java/app/BookingTest.java
@@ -4,19 +4,21 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.hasItems;
 
-public class BookingTest extends TestBase{
+public class BookingTest extends TestBase {
 
     BookingSteps steps;
-    public BookingTest(){
+
+    public BookingTest() {
         steps = new BookingSteps();
     }
+
     @Test
     public void getAllBookings() {
         steps
                 .get("/booking")
                 .statusCode(200)
                     .and()
-                .body("bookingid.findAll { it < 5}",hasItems(1,2,3,4));
+                .body("bookingid.findAll { it < 5}", hasItems(1, 2, 3, 4));
     }
 
 }
